0379cb7097a3fd15e4eba6e51cbde07dea50458c
hville <hugov@runbox.com>
2021-06-21 18:36:49 -0400

    update assert-op

--- test.js
+++ test.js
@@ -2,51 +2,51 @@
 import t from 'assert-op'
 import L from './index.js'
 
-//t('single set', () => {
-var stat = new L(1)
-t('===', stat.push(3), 1)
-t('===', stat.ave(), 3)
-t('===', isNaN(stat.var(0)), true)
-t('===', stat.push(1), 2)
-t('===', stat.ave(0), 2)
-t('===', stat.N, 2)
-t('===', stat.var(), 2)
+t('single set', a => {
+	const stat = new L(1)
+	a('===', stat.push(3), 1)
+	a('===', stat.ave(), 3)
+	a('===', isNaN(stat.var(0)), true)
+	a('===', stat.push(1), 2)
+	a('===', stat.ave(0), 2)
+	a('===', stat.N, 2)
+	a('===', stat.var(), 2)
+})
+t('2 dimensions...', a => {
+	const stat = new L(2)
+	a('===', stat.push(1,2), 1)
+	a('===', stat.push([2,1]), 2)
+	a('===', stat.ave(0), 3/2)
+	a('===', stat.ave(1), 3/2)
+	a('===', stat.var(1), 1/2)
+	a('===', stat.cor(0,1), -1)
+	a('===', stat.cor(0,0), 1)
+})
+t('3 dimensions...', a => {
+	const stat = new L(3)
+	a('===', stat.push(2,1,0), 1)
+	a('===', stat.push([1,1,1]), 2)
+	a('===', stat.push(0,1,2), 3)
+	a('===', stat.ave(0), 1)
+	a('===', stat.ave(1), 1)
+	a('===', stat.ave(2), 1)
+	a('===', stat.cov(0,0), 1)
+	a('===', stat.cov(1,0), 0)
+	a('===', stat.cov(1,2), 0)
+})
+t('stress test', a => {
+	const stat = new L(4)
+	for (var i=1; i<1001; ++i) {
+		stat.push(i, -i, i/2, 1)
+	}
+	a('===', stat.ave(0), 1001/2)
+	a('===', stat.ave(1), -1001/2)
+	a('===', stat.ave(2), 1001/4)
+	a('===', stat.ave(3), 1)
 
-//t('2 dimensions...', () => {
-var stat = new L(2)
-t('===', stat.push(1,2), 1)
-t('===', stat.push([2,1]), 2)
-t('===', stat.ave(0), 3/2)
-t('===', stat.ave(1), 3/2)
-t('===', stat.var(1), 1/2)
-t('===', stat.cor(0,1), -1)
-t('===', stat.cor(0,0), 1)
+	a('===', stat.cor(0,0), 1)
+	a('===', stat.cor(1,0), -1)
+	a('===', stat.cor(1,2), -1)
 
-//t('3 dimensions...', () => {
-var stat = new L(3)
-t('===', stat.push(2,1,0), 1)
-t('===', stat.push([1,1,1]), 2)
-t('===', stat.push(0,1,2), 3)
-t('===', stat.ave(0), 1)
-t('===', stat.ave(1), 1)
-t('===', stat.ave(2), 1)
-t('===', stat.cov(0,0), 1)
-t('===', stat.cov(1,0), 0)
-t('===', stat.cov(1,2), 0)
-
-//t('stress test', () => {
-var stat = new L(4)
-for (var i=1; i<1001; ++i) {
-	stat.push(i, -i, i/2, 1)
-}
-t('===', stat.ave(0), 1001/2)
-t('===', stat.ave(1), -1001/2)
-t('===', stat.ave(2), 1001/4)
-t('===', stat.ave(3), 1)
-
-t('===', stat.cor(0,0), 1)
-t('===', stat.cor(1,0), -1)
-t('===', stat.cor(1,2), -1)
-
-t('===', stat.cov(2,3), 0)
-
+	a('===', stat.cov(2,3), 0)
+})
